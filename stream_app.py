97d61eda5933b18e3f63d5a218f6ed05204770cb
Neurodium <jvandlife@gmail.com>
2023-04-10 23:25:12 +0200

    manage user not in db

--- stream_app.py
+++ stream_app.py
@@ -3,18 +3,21 @@ import requests
 import os
 
 
-url = os.environ['AZ_FUNC']
+url = "https://p9recarticles.azurewebsites.net/api/httptrigger1"
 
 
 st.write("Enter user ID:")
 user_id = st.number_input(label="User ID", min_value=0, format='%d')
 if st.button("Send"):
     params = {'user': user_id}
-    response = requests.get(url, params=params)
-    data = response.json()
-    st.write(f"Recommended articles for user {user_id}:")
-    for art_pos, article in data['articles'].items():
-        st.write(f"Article {art_pos}: {article}")
+    try:
+        response = requests.get(url, params=params)
+        data = response.json()
+        st.write(f"Recommended articles for user {user_id}:")
+        for art_pos, article in data['articles'].items():
+            st.write(f"Article {art_pos}: {article}")
+    except:
+        st.write(f"Incorrect User ID")
 
 
 
